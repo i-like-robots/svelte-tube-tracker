6e02364ae257598e64e14c9ebcebae573ad59e83
Matt Hinchliffe <matt@maketea.co.uk>
2021-02-07 21:36:08 +0000

    Inline HTML template into home page route handler

--- src/server/routes/homeRoute.js
+++ src/server/routes/homeRoute.js
@@ -1,5 +1,4 @@
 const { arrivals } = require('../lib/apiClient')
-const homeView = require('../views/homeView')
 const networkData = require('../../common/networkData.json')
 const App = require('../../components/App.svelte').default
 
@@ -14,14 +13,29 @@ async function homeRoute(request, response) {
     }
 
     const app = App.render({ networkData, initialData }).html
-    const html = homeView({ app, initialData })
 
     response.writeHead(200, {
       'Cache-Control': 'max-age=30, must-revalidate',
       'Content-Type': 'text/html; charset=UTF-8',
     })
 
-    response.end(html)
+    response.end(`<!DOCTYPE html>
+      <html lang="en-GB">
+        <head>
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <title>TfL London Underground Arrivals</title>
+          <link rel="stylesheet" href="/styles.css">
+        </head>
+        <body>
+          <div id="app">${app}</div>
+          <script type="application/json" id="initialData">
+            ${JSON.stringify(initialData)}
+          </script>
+          <script src="/bundle.js"></script>
+        </body>
+      </html>
+    `)
   } catch (error) {
     console.error(error)
     response.statusCode = error.status || 500
